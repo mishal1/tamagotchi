1de5afae68e2218add0f6bf404cf2efcca251346
Mishal Islam <mishal.islam01@gmail.com>
2015-02-19 16:59:20 +0000

    removed hatching process- maybe user should hatch egg creating a new tamagotchi

--- src/tamagotchi.js
+++ src/tamagotchi.js
@@ -1,11 +1,6 @@
  function Tamagotchi(){
-  this.hatched = false;
- }
-
- Tamagotchi.prototype.hatch = function() {
-   this.hatched = true;
    this.age = 0;
- };
+ }
 
  Tamagotchi.prototype.increaseAge = function() {
    this.age++;
--- test/unitTests/tamagotchiSpec.js
+++ test/unitTests/tamagotchiSpec.js
@@ -8,36 +8,18 @@ describe ('Tamagotchi', function(){
     tamagotchi = new Tamagotchi();
   });
 
-  it('is not hatched when initialized', function(){
-    expect(tamagotchi.hatched).toEqual(false)
+  it('has an age of 0', function(){
+    expect(tamagotchi.age).toEqual(0)
   });
 
-  describe('after it has been hatched', function(){
+  it('can age', function(){
+    tamagotchi.increaseAge()
+    expect(tamagotchi.age).toEqual(1)
+  });
 
-    var hatch;
-
-    beforeEach(function(){
-      hatch = tamagotchi.hatch()
-    });
-
-    it('hatched status changes', function(){
-      expect(tamagotchi.hatched).toEqual(true)
-    });
-
-    it('has an age of 0', function(){
-      expect(tamagotchi.age).toEqual(0)
-    });
-
-    it('can age', function(){
-      tamagotchi.increaseAge()
-      expect(tamagotchi.age).toEqual(1)
-    });
-
-    it('can set a name', function(){
-      tamagotchi.setName('Mishal')
-      expect(tamagotchi.name).toEqual('Mishal')
-    });
-
+  it('can set a name', function(){
+    tamagotchi.setName('Mishal')
+    expect(tamagotchi.name).toEqual('Mishal')
   });
 
 });
