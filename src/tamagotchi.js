cc319138dc71663a222b9832515a3ee568740b6f
Mishal Islam <mishal.islam01@gmail.com>
2015-02-20 14:31:21 +0000

    added passage of time

--- src/tamagotchi.js
+++ src/tamagotchi.js
@@ -2,6 +2,7 @@ function Tamagotchi(happiness, tiredness, fullness, hunger){
   this.age = 0;
   this.needs = [happiness, tiredness, fullness, hunger];
   this.alive = true;
+  setInterval(this.passageOfTime, 30000);
 }
 
 Tamagotchi.prototype.increaseAge = function() {
@@ -16,8 +17,15 @@ Tamagotchi.prototype.checkIfAlive = function() {
   if(this.checkAllNeeds()) this.alive = false;
 };
 
-Tamagotchi.prototype.checkAllNeeds = function(first_argument) {
+Tamagotchi.prototype.checkAllNeeds = function() {
   return this.needs[0].value === 0 && this.needs[1].value === 10 && this.needs[2].value === 10 && this.needs[3].value === 10;
+};
+
+Tamagotchi.prototype.passageOfTime = function() {
+  this.needs[0].decrease();
+  this.needs[1].increase();
+  this.needs[2].increase();
+  this.needs[3].increase();
 };
 
 module.exports = Tamagotchi;
--- test/unitTests/tamagotchiSpec.js
+++ test/unitTests/tamagotchiSpec.js
@@ -5,10 +5,26 @@ describe ('Tamagotchi', function(){
   var tamagotchi, happiness, tiredness, fullness, hunger;
 
   beforeEach(function(){
-    happiness = {value: 0}
-    tiredness = {value: 10}
-    fullness = {value: 10}
-    hunger = {value: 10}
+    happiness = {
+      value: 6, 
+      increase: function(){this.value += 2}, 
+      decrease: function(){this.value -= 2}
+    }
+    tiredness = {
+      value: 6, 
+      increase: function(){this.value += 2}, 
+      decrease: function(){this.value -= 2}
+    }
+    fullness = {
+      value: 6, 
+      increase: function(){this.value += 2}, 
+      decrease: function(){this.value -= 2}
+    }
+    hunger = {
+      value: 6, 
+      increase: function(){this.value += 2}, 
+      decrease: function(){this.value -= 2}
+    }
     tamagotchi = new Tamagotchi(happiness, tiredness, fullness, hunger);
   });
 
@@ -34,7 +50,39 @@ describe ('Tamagotchi', function(){
     expect(tamagotchi.alive).toEqual(true)
   });
 
+  describe('over time the tamagotchis needs will change', function(){
+
+    beforeEach(function(){
+      tamagotchi.passageOfTime()
+    });
+
+    it('the tamagotchis happiness decreases', function(){
+      expect(tamagotchi.needs[0].value).toEqual(4)
+    });
+
+    it('the tamagotchis tiredness increases', function(){
+      expect(tamagotchi.needs[1].value).toEqual(8)
+    });
+
+    it('the tamagotchis fullness increases', function(){
+      expect(tamagotchi.needs[2].value).toEqual(8)
+    });
+
+    it('the tamagotchis hunger increases', function(){
+      expect(tamagotchi.needs[3].value).toEqual(8)
+    });
+
+  });
+
   describe('if the value for all needs are 0', function(){
+
+    beforeEach(function(){
+      happiness = {value: 0}
+      tiredness = {value: 10}
+      fullness = {value: 10}
+      hunger = {value: 10}
+      tamagotchi = new Tamagotchi(happiness, tiredness, fullness, hunger);
+    });
 
     it('it will die', function(){
       tamagotchi.checkIfAlive()
