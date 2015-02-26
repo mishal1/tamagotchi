b558cb54afc67afee5f5974c78cd766de75177b7
Mishal Islam <mishal.islam01@gmail.com>
2015-02-26 11:27:45 +0000

    ajax practice

--- server.js
+++ server.js
@@ -2,12 +2,18 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
-app.use(express.static(__dirname + '/views'));
 
 app.get('/', function(request, response){
-  response.render('index.ejs')
+  response.render('index');
 });
+
+app.post('/name', function(req, res) {
+  var name = req.body.name;
+  res.send(name + 'funny')
+})
 
 server.listen(3000, function(){
   console.log("Server listening on port 3000");
