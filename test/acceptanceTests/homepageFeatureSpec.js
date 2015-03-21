41413dcb429f2786c8ac2f482d28c04858aed397
Mishal Islam <mishal.islam01@gmail.com>
2015-03-21 11:41:47 +0000

    asks user their name

--- test/acceptanceTests/homepageFeatureSpec.js
+++ test/acceptanceTests/homepageFeatureSpec.js
@@ -21,7 +21,7 @@ describe('Homepage', function() {
       .call(done);
   });
 
-  it('it displays a greeting after hatching', function(done){
+  it('displays a greeting after hatching', function(done){
     this.timeout(8000);
     client
       .url('http://localhost:3000')
@@ -33,6 +33,28 @@ describe('Homepage', function() {
     .call(done);
   });
 
-  
+  it('asks user to name their tamagotchi', function(done){
+    this.timeout(8000);
+    client
+      .url('http://localhost:3000')
+      .click('.egg')
+      .waitForVisible('#ask-name', 5000)
+      .getText('#ask-name', function(err, text){
+          expect(text).to.equal('What do you want to name me?');
+      })
+    .call(done);
+  });
+
+  it('a user can enter their name', function(done){
+    this.timeout(8000)
+    client
+      .url('http://localhost:3000')
+      .click('.egg')
+      .waitForVisible('#input', 5000)
+      .pause(2000)
+      .setValue('#input', 'Mishal')
+      .submit('#input')
+    .call(done);
+  });
 
 });
